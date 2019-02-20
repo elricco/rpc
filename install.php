67a9e1644ec2a96ab082f6e75c8156f6cbb7e6a3
Tim Filler <tim.filler@elricco.net>
2019-02-20 21:22:20 +0100

    Update table schemes and install.php

--- install.php
+++ install.php
@@ -2,3 +2,20 @@
 /** @var rex_addon $this */
 $vat_types = rex_file::get(rex_path::addon('rpc', 'install/tablesets/rpc_vat_types.json'));
 rex_yform_manager_table_api::importTablesets($vat_types);
+
+$general_prices = rex_file::get(rex_path::addon('rpc', 'install/tablesets/rpc_general_prices.json'));
+rex_yform_manager_table_api::importTablesets($general_prices);
+
+$paper_option = rex_file::get(rex_path::addon('rpc', 'install/tablesets/rpc_paper_option.json'));
+rex_yform_manager_table_api::importTablesets($paper_option);
+
+$fixation_option = rex_file::get(rex_path::addon('rpc', 'install/tablesets/rpc_fixation_option.json'));
+rex_yform_manager_table_api::importTablesets($fixation_option);
+
+$fixation_addition = rex_file::get(rex_path::addon('rpc', 'install/tablesets/rpc_fixation_addition.json'));
+rex_yform_manager_table_api::importTablesets($fixation_addition);
+
+$fixation_addition_options = rex_file::get(rex_path::addon('rpc', 'install/tablesets/rpc_fixation_addition_options.json'));
+rex_yform_manager_table_api::importTablesets($fixation_addition_options);
+
+
