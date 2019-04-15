3a59e9c8cb2c89ca06f507f773203fde3c3527ac
Tim Filler <tim.filler@elricco.net>
2019-04-15 20:47:18 +0200

    limit init scope of js to config_form

--- _source/assets/scripts/print_configurator.js
+++ _source/assets/scripts/print_configurator.js
@@ -45,86 +45,87 @@ $(function () {
             
         });
     }
-
-    // set range slider for black and white pages
-    let slider_baw = document.getElementById('page_baw');
-    let $slider_baw = $('#page_baw');
-    let input_baw_id = $slider_baw.next('div').find('input').prop('id');
-    let input_baw = document.getElementById(input_baw_id);
-    noUiSlider.create(slider_baw, {
-        start: [ $slider_baw.next('div').find('input').data('start') ],
-        step: 1,
-        connect: [ true,false ],
-        range: {
-            'min': [ $slider_baw.next('div').find('input').data('min') ],
-            'max': [ $slider_baw.next('div').find('input').data('max') ]
-        },
-    });
-
-    // set range slider for coloured pages
-    let slider_clr = document.getElementById('page_clr');
-    let $slider_clr = $('#page_clr');
-    let input_clr_id = $slider_clr.next('div').find('input').prop('id');
-    let input_clr = document.getElementById(input_clr_id);
-    noUiSlider.create(slider_clr, {
-        start: [ $slider_clr.next('div').find('input').data('start') ],
-        step: 1,
-        connect: [ true,false ],
-        range: {
-            'min': [ $slider_clr.next('div').find('input').data('min') ],
-            'max': [ $slider_clr.next('div').find('input').data('max') ]
-        }
-    });
-
-    // set update listener
-    slider_baw.noUiSlider.on('set', function( values, handle ) {
-        input_baw.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
-        slider_clr.noUiSlider.updateOptions({
-            range: {
-                'min': [ $slider_clr.next('div').find('input').data('min') ],
-                'max': [ Number.parseInt(input_baw.value) ]
-            }
-        });
-        calculate_price();
-    });
-    input_baw.addEventListener('keyup', function () {
-        slider_baw.noUiSlider.set(this.value);
-        slider_clr.noUiSlider.updateOptions({
-            range: {
-                'min': [ $slider_clr.next('div').find('input').data('min') ],
-                'max': [ Number.parseInt(input_baw.value) ]
-            }
-        });
-        calculate_price();
-    });
-
-    // set update listener
-    slider_clr.noUiSlider.on('set', function( values, handle ) {
-        input_clr.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
-        calculate_price();
-    });
-    input_clr.addEventListener('keyup', function () {
-        slider_clr.noUiSlider.set(this.value);
-        calculate_price();
-    });
-
-    // set update listener
-    $('input:radio').on('click',function () {
-        $.each($('input:radio:checked'),function (index, value) {
-                calculate_price();
-        });
-    });
-
-    // set update listener
-    $('input[name*="fixation"]').on('keyup', function () {
-        calculate_price();
-    });
-
     function init_calculator() {
         getBasics();
+
+        // set range slider for black and white pages
+        let slider_baw = document.getElementById('page_baw');
+        let $slider_baw = $('#page_baw');
+        let input_baw_id = $slider_baw.next('div').find('input').prop('id');
+        let input_baw = document.getElementById(input_baw_id);
+        noUiSlider.create(slider_baw, {
+            start: [ $slider_baw.next('div').find('input').data('start') ],
+            step: 1,
+            connect: [ true,false ],
+            range: {
+                'min': [ $slider_baw.next('div').find('input').data('min') ],
+                'max': [ $slider_baw.next('div').find('input').data('max') ]
+            },
+        });
+
+        // set range slider for coloured pages
+        let slider_clr = document.getElementById('page_clr');
+        let $slider_clr = $('#page_clr');
+        let input_clr_id = $slider_clr.next('div').find('input').prop('id');
+        let input_clr = document.getElementById(input_clr_id);
+        noUiSlider.create(slider_clr, {
+            start: [ $slider_clr.next('div').find('input').data('start') ],
+            step: 1,
+            connect: [ true,false ],
+            range: {
+                'min': [ $slider_clr.next('div').find('input').data('min') ],
+                'max': [ $slider_clr.next('div').find('input').data('max') ]
+            }
+        });
+
+        // set update listener
+        slider_baw.noUiSlider.on('set', function( values, handle ) {
+            input_baw.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
+            slider_clr.noUiSlider.updateOptions({
+                range: {
+                    'min': [ $slider_clr.next('div').find('input').data('min') ],
+                    'max': [ Number.parseInt(input_baw.value) ]
+                }
+            });
+            calculate_price();
+        });
+        input_baw.addEventListener('keyup', function () {
+            slider_baw.noUiSlider.set(this.value);
+            slider_clr.noUiSlider.updateOptions({
+                range: {
+                    'min': [ $slider_clr.next('div').find('input').data('min') ],
+                    'max': [ Number.parseInt(input_baw.value) ]
+                }
+            });
+            calculate_price();
+        });
+
+        // set update listener
+        slider_clr.noUiSlider.on('set', function( values, handle ) {
+            input_clr.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
+            calculate_price();
+        });
+        input_clr.addEventListener('keyup', function () {
+            slider_clr.noUiSlider.set(this.value);
+            calculate_price();
+        });
+
+        // set update listener
+        $('input:radio').on('click',function () {
+            $.each($('input:radio:checked'),function (index, value) {
+                    calculate_price();
+            });
+        });
+
+        // set update listener
+        $('input[name*="fixation"]').on('keyup', function () {
+            calculate_price();
+        });
+
         replaceTextWithHtml($('[name*="paper_options"]').next());
         calculate_price();
     }
-
-    init_calculator();
+    if($('#config_form').length) {
+        init_calculator();
+    }
 });
--- assets/js/print_configurator.js
+++ assets/js/print_configurator.js
@@ -37,86 +37,87 @@ $(function () {
       $('.order-subtotal').html(data['dom_elements']['order-subtotal']);
       $('.order-fixations').html(data['dom_elements']['order-fixations']);
     }).fail(function () {});
-  } // set range slider for black and white pages
-
-
-  var slider_baw = document.getElementById('page_baw');
-  var $slider_baw = $('#page_baw');
-  var input_baw_id = $slider_baw.next('div').find('input').prop('id');
-  var input_baw = document.getElementById(input_baw_id);
-  noUiSlider.create(slider_baw, {
-    start: [$slider_baw.next('div').find('input').data('start')],
-    step: 1,
-    connect: [true, false],
-    range: {
-      'min': [$slider_baw.next('div').find('input').data('min')],
-      'max': [$slider_baw.next('div').find('input').data('max')]
-    }
-  }); // set range slider for coloured pages
-
-  var slider_clr = document.getElementById('page_clr');
-  var $slider_clr = $('#page_clr');
-  var input_clr_id = $slider_clr.next('div').find('input').prop('id');
-  var input_clr = document.getElementById(input_clr_id);
-  noUiSlider.create(slider_clr, {
-    start: [$slider_clr.next('div').find('input').data('start')],
-    step: 1,
-    connect: [true, false],
-    range: {
-      'min': [$slider_clr.next('div').find('input').data('min')],
-      'max': [$slider_clr.next('div').find('input').data('max')]
-    }
-  }); // set update listener
-
-  slider_baw.noUiSlider.on('set', function (values, handle) {
-    input_baw.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
-
-    slider_clr.noUiSlider.updateOptions({
-      range: {
-        'min': [$slider_clr.next('div').find('input').data('min')],
-        'max': [Number.parseInt(input_baw.value)]
-      }
-    });
-    calculate_price();
-  });
-  input_baw.addEventListener('keyup', function () {
-    slider_baw.noUiSlider.set(this.value);
-    slider_clr.noUiSlider.updateOptions({
-      range: {
-        'min': [$slider_clr.next('div').find('input').data('min')],
-        'max': [Number.parseInt(input_baw.value)]
-      }
-    });
-    calculate_price();
-  }); // set update listener
-
-  slider_clr.noUiSlider.on('set', function (values, handle) {
-    input_clr.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
-
-    calculate_price();
-  });
-  input_clr.addEventListener('keyup', function () {
-    slider_clr.noUiSlider.set(this.value);
-    calculate_price();
-  }); // set update listener
-
-  $('input:radio').on('click', function () {
-    $.each($('input:radio:checked'), function (index, value) {
-      calculate_price();
-    });
-  }); // set update listener
-
-  $('input[name*="fixation"]').on('keyup', function () {
-    calculate_price();
-  });
+  }
 
   function init_calculator() {
-    getBasics();
+    getBasics(); // set range slider for black and white pages
+
+    var slider_baw = document.getElementById('page_baw');
+    var $slider_baw = $('#page_baw');
+    var input_baw_id = $slider_baw.next('div').find('input').prop('id');
+    var input_baw = document.getElementById(input_baw_id);
+    noUiSlider.create(slider_baw, {
+      start: [$slider_baw.next('div').find('input').data('start')],
+      step: 1,
+      connect: [true, false],
+      range: {
+        'min': [$slider_baw.next('div').find('input').data('min')],
+        'max': [$slider_baw.next('div').find('input').data('max')]
+      }
+    }); // set range slider for coloured pages
+
+    var slider_clr = document.getElementById('page_clr');
+    var $slider_clr = $('#page_clr');
+    var input_clr_id = $slider_clr.next('div').find('input').prop('id');
+    var input_clr = document.getElementById(input_clr_id);
+    noUiSlider.create(slider_clr, {
+      start: [$slider_clr.next('div').find('input').data('start')],
+      step: 1,
+      connect: [true, false],
+      range: {
+        'min': [$slider_clr.next('div').find('input').data('min')],
+        'max': [$slider_clr.next('div').find('input').data('max')]
+      }
+    }); // set update listener
+
+    slider_baw.noUiSlider.on('set', function (values, handle) {
+      input_baw.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
+
+      slider_clr.noUiSlider.updateOptions({
+        range: {
+          'min': [$slider_clr.next('div').find('input').data('min')],
+          'max': [Number.parseInt(input_baw.value)]
+        }
+      });
+      calculate_price();
+    });
+    input_baw.addEventListener('keyup', function () {
+      slider_baw.noUiSlider.set(this.value);
+      slider_clr.noUiSlider.updateOptions({
+        range: {
+          'min': [$slider_clr.next('div').find('input').data('min')],
+          'max': [Number.parseInt(input_baw.value)]
+        }
+      });
+      calculate_price();
+    }); // set update listener
+
+    slider_clr.noUiSlider.on('set', function (values, handle) {
+      input_clr.value = Number.parseFloat(values[handle]).toFixed(0); // convert number to no decimals
+
+      calculate_price();
+    });
+    input_clr.addEventListener('keyup', function () {
+      slider_clr.noUiSlider.set(this.value);
+      calculate_price();
+    }); // set update listener
+
+    $('input:radio').on('click', function () {
+      $.each($('input:radio:checked'), function (index, value) {
+        calculate_price();
+      });
+    }); // set update listener
+
+    $('input[name*="fixation"]').on('keyup', function () {
+      calculate_price();
+    });
     replaceTextWithHtml($('[name*="paper_options"]').next());
     calculate_price();
   }
 
-  init_calculator();
+  if ($('#config_form').length) {
+    init_calculator();
+  }
 });
 
 },{"nouislider":2}],2:[function(require,module,exports){
